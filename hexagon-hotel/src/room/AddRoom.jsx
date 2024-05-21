f00f4bede8dbff8bc2d87aff6ec2fc0b488d9495
Banjoko A.L <56545809+Habay88@users.noreply.github.com>
2024-05-21 21:26:40 +0100

    WIP

--- hexagon-hotel/src/room/AddRoom.jsx
+++ hexagon-hotel/src/room/AddRoom.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
 const AddRoom = () => {
+    const [newRoom, setNewRoom] = useState({
+        photo: null,
+        roomType: "",
+        roomPrice: ""
+    })
+    const[imagePreview,setImagePreview]= useState("")
+    const [successMessage,setSuccessMessage] = useState("") 
+    const [errorMessage,setErrorMessage] = useState("")
+
+    const handleRoomInputChange = (e)=> {
+        const name =e.target.name
+        let value = e.target.value
+        if(name === "roomPrice") {
+            if(!isNaN(value)) {
+                value.parseInt(value)
+            }else{
+                value = ""
+            }
+        }
+        setNewRoom(...newRoom,[name]: value)
+    }
   return (
-    <div>AddRoom</div>
+    <div>
+    
+    
+    </div>
   )
 }
 
