c568fd645cd517101d6b74d92f3aceef81b057c1
Banjoko A.L <56545809+Habay88@users.noreply.github.com>
2024-05-22 21:54:24 +0100

    COMPONENT ROOM TYP SELECTOR

---
+++ hexagon-hotel/src/commons/RoomTypeSelector.jsx
@@ -0,0 +1,19 @@
+import { use } from 'express/lib/application'
+import React , {useEffect, useState}from 'react'
+
+const RoomTypeSelector = () => {
+    const [RoomTypes,setRoomTypes] = useState([])
+    const [showNewRoomTypeInput,setShowNewRoomTypesInput] = useState(false)
+    const [newRomType,setNewRoomType] = useState("")
+ 
+ useEffect(() => {
+    
+ })
+    return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default RoomTypeSelector
